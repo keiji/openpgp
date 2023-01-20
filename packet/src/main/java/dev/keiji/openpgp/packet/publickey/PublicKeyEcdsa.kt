487fac92095884a9955e507604f0a6e1f3045361
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2023-01-21 02:34:27 +0900

    Add PublicKey[Ecdsa|Eddsa]Test

--- packet/src/main/java/dev/keiji/openpgp/packet/publickey/PublicKeyEcdsa.kt
+++ packet/src/main/java/dev/keiji/openpgp/packet/publickey/PublicKeyEcdsa.kt
@@ -46,4 +46,25 @@ class PublicKeyEcdsa : PublicKey() {
     * ecPoint: ${ecPoint?.toHex()}
         """.trimIndent()
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PublicKeyEcdsa
+
+        if (ellipticCurveParameter != other.ellipticCurveParameter) return false
+        if (ecPoint != null) {
+            if (other.ecPoint == null) return false
+            if (!ecPoint.contentEquals(other.ecPoint)) return false
+        } else if (other.ecPoint != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = ellipticCurveParameter?.hashCode() ?: 0
+        result = 31 * result + (ecPoint?.contentHashCode() ?: 0)
+        return result
+    }
 }
---
+++ packet/src/test/java/dev/keiji/openpgp/packet/publickey/PublicKeyEddsaTest.kt
@@ -0,0 +1,47 @@
+package dev.keiji.openpgp.packet.publickey
+
+import dev.keiji.openpgp.EllipticCurveParameter
+import dev.keiji.openpgp.SymmetricKeyAlgorithm
+import dev.keiji.openpgp.parseHexString
+import dev.keiji.openpgp.toHex
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
+class PublicKeyEddsaTest {
+
+    val binary = "092B06010401DA470F01" +
+            "0029010203040506"
+
+    @Test
+    fun testEncode() {
+        val expected = binary
+        val data = PublicKeyEddsa().also {
+            it.ellipticCurveParameter = EllipticCurveParameter.Ed25519
+            it.ecPoint = byteArrayOf(1, 2, 3, 4, 5, 6)
+        }
+
+        val actual = ByteArrayOutputStream().let {
+            data.writeTo(it)
+            it.toByteArray()
+        }
+
+        assertEquals(expected, actual.toHex(""))
+    }
+
+    @Test
+    fun testDecode() {
+        val expected = PublicKeyEddsa().also {
+            it.ellipticCurveParameter = EllipticCurveParameter.Ed25519
+            it.ecPoint = byteArrayOf(1, 2, 3, 4, 5, 6)
+        }
+        val data = parseHexString(binary)
+        val actual = PublicKeyEddsa().also {
+            it.readFrom(ByteArrayInputStream(data))
+        }
+
+        assertTrue(expected.equals(actual))
+    }
+}
