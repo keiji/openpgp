bb2a2e9f287d809dfe7fa309944eb152f81931ca
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2023-02-27 09:17:00 +0900

    Detect ASCII Armor form.

--- sample/src/main/java/dev/keiji/openpgp/sample/Main.kt
+++ sample/src/main/java/dev/keiji/openpgp/sample/Main.kt
@@ -5,6 +5,7 @@ import dev.keiji.openpgp.packet.PacketDecoder
 import dev.keiji.openpgp.packet.Tag
 import dev.keiji.openpgp.packet.signature.PacketSignature
 import dev.keiji.openpgp.toHex
+import java.io.ByteArrayInputStream
 import java.io.File
 import java.io.FileInputStream
 import java.nio.charset.StandardCharsets
@@ -24,8 +25,13 @@ fun main(args: Array<String>) {
     }
 
     val packetList = FileInputStream(file).use {
-        val encoded = String(it.readAllBytes(), StandardCharsets.UTF_8)
-        PacketDecoder.decode(encoded)
+        val bytes = it.readAllBytes()
+        val encoded = String(bytes, StandardCharsets.UTF_8)
+        if (isAsciiArmoredForm(encoded)) {
+            PacketDecoder.decode(encoded)
+        } else {
+            PacketDecoder.decode(ByteArrayInputStream(bytes))
+        }
     }
 
     packetList.forEach { packet ->
@@ -38,6 +44,35 @@ fun main(args: Array<String>) {
     } else {
         verify(packetList)
     }
+}
+
+fun isAsciiArmoredForm(encoded: String): Boolean {
+    val lines = encoded.lines()
+    if (lines.size < 4) {
+        return false
+    }
+
+    if (lines[1].isNotBlank()) {
+        return false
+    }
+
+    val linesNotBlank = lines.filter { it.isNotBlank() }
+
+    if (!linesNotBlank.first().startsWith("-----") || !linesNotBlank.first().endsWith("-----")) {
+        return false
+    }
+
+    if (!linesNotBlank.last().startsWith("-----") || !linesNotBlank.last().endsWith("-----")) {
+        return false
+    }
+
+    val content = linesNotBlank
+        .subList(1, lines.size - 2)
+        .joinToString("")
+    println(content)
+
+    return true
+
 }
 
 private fun verify(packetList: List<Packet>) {
