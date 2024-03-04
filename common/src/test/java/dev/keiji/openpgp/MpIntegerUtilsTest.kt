9682975ab582818c0f33bc364d6c2026fe93720b
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2024-03-05 00:31:45 +0900

    Resolve detekt error

--- common/src/test/java/dev/keiji/openpgp/MpIntegerUtilsTest.kt
+++ common/src/test/java/dev/keiji/openpgp/MpIntegerUtilsTest.kt
@@ -1,6 +1,10 @@
 package dev.keiji.openpgp
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Test
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
