66bc3425891a0d8264baf20d8c839071eff974b3
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2024-03-05 00:16:07 +0900

    Setup Detekt

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ pluginManagement {
     plugins {
         kotlin("jvm") version "1.8.10"
         id("org.jetbrains.dokka") version "1.8.10"
+        id("io.gitlab.arturbosch.detekt") version "1.23.5"
     }
 }
 
