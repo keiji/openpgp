f7c1eba1df91f4258f33555beb98063b51c84196
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2024-03-05 23:49:57 +0900

    Resolve detekt error

--- signature-ext/src/main/java/dev/keiji/openpgp/packet/publickey/PublicKeyEddsaExtensions.kt
+++ signature-ext/src/main/java/dev/keiji/openpgp/packet/publickey/PublicKeyEddsaExtensions.kt
@@ -3,6 +3,7 @@ package dev.keiji.openpgp.packet.publickey
 import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters
 
 fun PublicKeyEddsa.toBouncycastlePublicKey(): Ed25519PublicKeyParameters {
+    @Suppress("MagicNumber")
     val buffer = ecPoint?.copyOfRange(1, 33)
     return Ed25519PublicKeyParameters(buffer)
 }
